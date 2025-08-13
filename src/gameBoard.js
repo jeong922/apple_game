517e12594dc23b27f731575238d155004ee0bfa2
jeong922 <hwajeong922@naver.com>
2025-08-13 14:03:14 +0900

    Refactor: 게임보드 SVG 아이콘 렌더링을 renderIcon() 메서드로 분리

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -18,6 +18,15 @@ export default class GameBoard {
     };
   }
 
+  renderIcon() {
+    return `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path d="M224 112c-8.8 0-16-7.2-16-16V80c0-44.2 35.8-80 80-80h16c8.8 0 16 7.2 16 16v16c0 44.2-35.8 80-80 80h-16z" fill="#B5E48C"/>
+        <path d="M0 288c0-76.3 35.7-160 112-160c27.3 0 59.7 10.3 82.7 19.3c18.8 7.3 39.9 7.3 58.7 0c22.9-8.9 55.4-19.3 82.7-19.3c76.3 0 112 83.7 112 160c0 128-80 224-160 224c-16.5 0-38.1-6.6-51.5-11.3c-8.1-2.8-16.9-2.8-25 0c-13.4 4.7-35 11.3-51.5 11.3C80 512 0 416 0 288z" fill="#FF6B6B"/>
+      </svg>
+    `;
+  }
+
   template() {
     return `
     <section class="board">
@@ -28,16 +37,7 @@ export default class GameBoard {
               .map(
                 (num, colIndex) =>
                   `<div class="cell" data-row="${rowIndex}" data-col="${colIndex}">
-                    ${
-                      num
-                        ? `
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path d="M224 112c-8.8 0-16-7.2-16-16V80c0-44.2 35.8-80 80-80h16c8.8 0 16 7.2 16 16v16c0 44.2-35.8 80-80 80h-16z" fill="#B5E48C"/>
-                        <path d="M0 288c0-76.3 35.7-160 112-160c27.3 0 59.7 10.3 82.7 19.3c18.8 7.3 39.9 7.3 58.7 0c22.9-8.9 55.4-19.3 82.7-19.3c76.3 0 112 83.7 112 160c0 128-80 224-160 224c-16.5 0-38.1-6.6-51.5-11.3c-8.1-2.8-16.9-2.8-25 0c-13.4 4.7-35 11.3-51.5 11.3C80 512 0 416 0 288z" fill="#FF6B6B"/>
-                      </svg>
-                    `
-                        : ''
-                    }
+                    ${num ? this.renderIcon() : ''}
                     <span>${num === 0 ? '' : num}</span>
                   </div>`
               )
@@ -99,6 +99,7 @@ export default class GameBoard {
 
     this.state.endRow = Number(cell.dataset.row);
     this.state.endCol = Number(cell.dataset.col);
+
     this.selectCells();
   };
 
